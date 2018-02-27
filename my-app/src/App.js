f2e8f2422de211bbc09134b93d1f285e353c6b78
Adrian Brown <adrianb@bighuman.com>
2018-02-27 16:01:48 -0500

    feature: redux store changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    console.log('HELLO WORLD', this);
     return (
       <div className="App">
         <header className="App-header">
