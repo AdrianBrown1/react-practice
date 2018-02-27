a877fb0460395a4184a7aa2ef8fd3d473aeed5da
Adrian Brown <adrianb@bighuman.com>
2018-02-27 14:52:46 -0500

    feature: installed Redux & created Store

---
+++ my-app/src/store/configureStore.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
+import loggingMiddleware from './middleware/logging';
+
+export default function configureStore(initialState) {
+  let middleware;
+
+  if (process.env.NODE_ENV !== 'production') {
+    middleware = applyMiddleware(thunk, loggingMiddleware);
+  } else {
+    middleware = applyMiddleware(thunk);
+  }
+  return createStore(rootReducer, initialState, middleware);
+}
