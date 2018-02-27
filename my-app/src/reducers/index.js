131016585076b735f42d7acaff92889587e594c8
Adrian Brown <adrianb@bighuman.com>
2018-02-27 17:04:34 -0500

    feature: created reducer

---
+++ my-app/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  // mapData: mapDataReducer,       <- example of using redux to map reducer.
+});
+
+export default rootReducer;
