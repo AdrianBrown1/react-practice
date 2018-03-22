955b9d9badbc0a04c034207c824464c3cdcff5bd
Adrian Brown <you@example.com>
2018-03-22 15:28:45 -0400

    refactor: added new actions for downloading weather info

--- my-app/src/actions/actions.js
+++ my-app/src/actions/actions.js
@@ -1,15 +1,48 @@
-import { ADD_ITEM } from '../constants/action-types';
+import { 
+  FETCH_WEATHER_REQUESTS, 
+  FETCH_WEATHER_ERR,
+  FETCH_WEATHER_SUCCESS,
+  API_KEY,
+} from '../constants/action-types';
+import { BASE_URL } from '../constants/weather-url';
 
-export const updateItems = (items) => ({
-  type: ADD_ITEM,
+export const fetchWeatherRequest = (url) => ({
+  type: FETCH_WEATHER_REQUESTS,
   payload: {
-    items,
+    url,
   },
 });
 
-export const addItem  = (item, items) => (dispatch) => {
-  console.log('*** ITEM ***', item);
-  console.log('   &&& ITEMs &&& ', items);
-  items.push(item);
-  dispatch(updateItems(items));
+export const fetchWeatherError = (err) => ({
+  type: FETCH_WEATHER_ERR,
+  payload: {
+    err,
+  },
+});
+
+export const fetchCurrentWeatherSuccess = (todaysWeather) => ({
+  type: FETCH_WEATHER_SUCCESS,
+  payload: {
+    todaysWeather,
+  },
+});
+
+export const fetchWeather = (param) => (dispatch) => {
+  const url = BASE_URL + param + API_KEY;
+  dispatch(fetchWeatherRequest(url));
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      const todaysWeather = {
+        description: data.weather[0].description,
+        currentTemp: data.main.temp,
+        maxTemp: data.main.temp_max,
+        minTemp: data.main.temp_min,
+        humidity: data.main.humidity,
+      }
+      dispatch(fetchCurrentWeatherSuccess(todaysWeather));
+    })
+    .catch((err) => { dispatch(fetchWeatherError(err)); });
 };
